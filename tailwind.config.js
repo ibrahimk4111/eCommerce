65982b9ce686b0d8c769b70a6a4719a43c6692de
ibrahimk4111 <ibrahimk4111@gmail.com>
2024-06-30 22:40:02 +0600

    package installed

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: 'selector',
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "custom-color": "#0b3631",
+      },
+      container: {
+        center: true,
+        padding: "1rem",
+        screens: {
+          sm: "100%",
+          md: "1050px",
+          lg: "1100px",
+          xxl: "1200px",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
